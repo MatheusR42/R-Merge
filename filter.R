ac433776a97acdb9141382db31c476756d3a1940
MatheusR42 <matheus.araujo.r42@gmail.com>
2017-09-18 22:32:13 -0300

    using merge function

--- filter.R
+++ filter.R
@@ -1,4 +1,4 @@
 text1 <- read.table('./text1.txt', sep = '|', header = FALSE, na.strings = "N/A", fill = TRUE)
 text2 <- read.table('./text2.txt', sep = '|', header = FALSE, na.strings = "N/A", fill = TRUE)
 
-merge <- text1[text1$V1 %in% text2$V1, ]
+merged <- merge(text1, text2, by = "V1")
