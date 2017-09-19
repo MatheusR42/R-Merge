9a065977cec5466ce970922a465267b1ce5cdadf
MatheusR42 <matheus.araujo.r42@gmail.com>
2017-09-18 22:05:19 -0300

    different collum number

--- filter.R
+++ filter.R
@@ -1,4 +1,4 @@
-text1 <- read.table('./text1.txt', sep = '|', header = FALSE)
-text2 <- read.table('./text2.txt', sep = '|', header = FALSE)
+text1 <- read.table('./text1.txt', sep = '|', header = FALSE, na.strings = "N/A", fill = TRUE)
+text2 <- read.table('./text2.txt', sep = '|', header = FALSE, na.strings = "N/A", fill = TRUE)
 
 merge <- text1[text1$V1 %in% text2$V1, ]
